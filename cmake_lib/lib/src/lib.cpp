ddf1ecd0631a17a1c875104af010f2ac093d0de3
wei chen <wei.chen@grabtaxi.com>
2023-08-02 18:26:54 +0800

    init

---
+++ cmake_lib/lib/src/lib.cpp
@@ -0,0 +1,7 @@
+#include <lib.h>
+
+#include <stdio.h>
+
+void helloLib() {
+    printf("hello lib\n");
+}
---
+++ cmake_lib/main.cpp
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+#include <lib.h>
+// #include <freetype.h>
+// #include <freetype2/ft2build.h>
+
+int main() {
+    
+
+    helloLib();
+
+    printf("\n####\n###\nhello world\n");
+
+    return 0;
+}
